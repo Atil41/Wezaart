44fded19aa0eab510f33470ff4a01843f9304161
David <David@10.66.125.87>
2015-02-05 13:53:30 +0100

    [Update] Finition page Home

--- js/custom.js
+++ js/custom.js
@@ -15,20 +15,17 @@ jQuery(document).ready(function ($) {
                 // itemsTablet: false,
                 // itemsMobile : false
     });
-
     $("#owl-artistes").owlCarousel({
         items: 6, //10 items above 1000px browser width
         itemsDesktop: [1000, 1], //5 items between 1000px and 901px
         itemsDesktopSmall: [900, 1], // betweem 900px and 601px
         itemsTablet: [600, 2], //2 items between 600 and 0
         itemsMobile: false, // itemsMobile disabled - inherit from itemsTablet option
-        autoPlay: true,
+        navigation: true,
+        autoPlay: false,
         pagination: false
-
     });
     $('#content').toggleClass("navhide").toggleClass("col-md-10").toggleClass("col-md-12");
-
-
     $('#navhidebtn').click(function () {
         $('#content').removeClass('notransition').toggleClass("navhide").toggleClass("col-md-10").toggleClass("col-md-12");
     });
