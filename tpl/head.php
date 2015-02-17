6235a6f8838075483b8ef053e956e17e62270b19
David <David@localhost>
2015-02-17 21:19:23 +0100

    résolution bug owl caroussel

--- tpl/head.php
+++ tpl/head.php
@@ -10,7 +10,7 @@
     <script src="../js/jquery.mixitup.min.js"></script>
     <script src="../js/custom.js"></script>
 
-	<link rel="stylesheet" type="text/css" href="../css/reset.css"/>
+    <link rel="stylesheet" type="text/css" href="../css/reset.css"/>
     <link rel="stylesheet" type="text/css" href="../css/intro.css"/>
     <link rel="stylesheet" type="text/css" href="../css/superslides.css"/>
     <script src="../jsintro/jquery.superslides.min.js"></script>
@@ -25,7 +25,7 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
       <![endif]-->
-	<link rel="stylesheet" href="../css/magnific-popup.css">  
+    <link rel="stylesheet" href="../css/magnific-popup.css">
     <link rel="stylesheet" href="../css/sliderskin.css">
     <link rel="stylesheet" href="../css/bootstrap.min.css">
     <link rel="stylesheet" href="../css/fontello/fontello.css">
